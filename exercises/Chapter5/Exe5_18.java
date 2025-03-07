5e3df170b215eafce776a1628e716900f523552a
Hesookim <hesookimaratozaraki@gmail.com>
2025-03-07 19:12:42 +0330

    commit

---
+++ exercises/Chapter5/Exe5_18.java
@@ -0,0 +1,43 @@
+package exercises.Chapter5;
+/*Question 5.18 :
+ Write a program that reads in three strings and sorts them lexicographically.
+Enter three strings:Charlie Able Baker
+Able
+Baker
+Charlie
+*/
+import java.util.Scanner;
+
+public class Exe5_18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter three strings : ");
+
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+        String str3 = scanner.next();
+
+        String temp;
+        if (str1.compareTo(str2) > 0 ) {
+            temp = str1;
+            str1 = str2;
+            str2 = temp;
+        }
+
+        if (str2.compareTo(str3) > 0 ) {
+            temp = str2;
+            str2 = str3;
+            str3 = temp;
+        }
+        if (str1.compareTo(str3) > 0 ) {
+            temp = str1;
+            str1 = str3;
+            str3 = temp;
+        }
+
+        System.out.println("After sorting the strings : ");
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
