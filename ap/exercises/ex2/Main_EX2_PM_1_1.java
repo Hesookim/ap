93d2e139f04300f8c123a8663ae7464170adc8b6
Hesookim <hesookimaratozaraki@gmail.com>
2025-03-25 20:44:21 +0330

    EX2_PM_1_1

--- ap/exercises/ex2/Main_EX2_PM_1_1.java
+++ ap/exercises/ex2/Main_EX2_PM_1_1.java
@@ -1,17 +1,21 @@
 package ap.exercises.ex2;
 
 import java.util.Scanner;
+
 public class Main_EX2_PM_1_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         // get input number from user
         System.out.print("Please enter the size of square: ");
 
         int k = scanner.nextInt();
         int i = k + 2;
+
         if (k <= 0) {
             System.out.println("Invalid number");
-        } else {
+        }
+        else {
             for (int r = 1; r <= i; r++) {//for row
                 for (int c = 1; c <= i; c++) {//for column
                     if (r == 1 || r == i || c == 1 || c == i) {
