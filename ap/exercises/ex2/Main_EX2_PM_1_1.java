f0b1f610ce0e259799a778f9295441ec6aefd491
Hesookim <hesookimaratozaraki@gmail.com>
2025-03-25 20:30:41 +0330

    EX2_PM_1_1

--- ap/exercises/ex2/Main_EX2_PM_1_1.java
+++ ap/exercises/ex2/Main_EX2_PM_1_1.java
@@ -9,17 +9,20 @@ public class Main_EX2_PM_1_1 {
 
         int k = scanner.nextInt();
         int i = k + 2;
-        for (int r = 1; r <= i; r++){//for row
-            for (int c = 1; c <= i; c++){//for column
-                if (r == 1 || r == i || c == 1 || c == i){
-                    System.out.print("*");
+        if (k <= 0) {
+            System.out.println("Invalid number");
+        } else {
+            for (int r = 1; r <= i; r++) {//for row
+                for (int c = 1; c <= i; c++) {//for column
+                    if (r == 1 || r == i || c == 1 || c == i) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
-                else {
-                    System.out.print(" ");
-                }
+                System.out.println();//after each row is done, move to the nest line
             }
-            System.out.println();//after each row is done, move to the nest line
+            scanner.close();
         }
-        scanner.close();
     }
 }
