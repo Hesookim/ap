2079bbc8b36a58437dc0743366ac1b4b7729b6ba
Hesookim <hesookimaratozaraki@gmail.com>
2025-03-25 19:56:03 +0330

    EX2_PM_1_1

---
+++ ap/exercises/ex2/Main_EX2_PM_1_1.java
@@ -0,0 +1,25 @@
+package ap.exercises.ex2;
+
+import java.util.Scanner;
+public class Main_EX2_PM_1_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // get input number from user
+        System.out.print("Please enter the size of square: ");
+
+        int k = scanner.nextInt();
+        int i = k + 2;
+        for (int r = 1; r <= i; r++){//for row
+            for (int c = 1; c <= i; c++){//for column
+                if (r == 1 || r == i || c == 1 || c == i){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();//after each row is done, move to the nest line
+        }
+        scanner.close();
+    }
+}
