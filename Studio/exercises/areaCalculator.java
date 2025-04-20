c20c69a4f5920d1013e9a99e7633e818bd7d373c
Hesookim <hesookimaratozaraki@gmail.com>
2025-04-20 09:11:21 +0330

    commit

--- Studio/exercises/areaCalculator.java
+++ Studio/exercises/areaCalculator.java
@@ -13,7 +13,9 @@ class Circle extends Shape{
     public Circle(double r) {
         radius = r;
 
-    }@Override
+    }
+
+    @Override
     public double area() {
         return Math.PI * radius * radius;
     }
@@ -32,12 +34,41 @@ class Rectangle extends Shape {
         return length * width;
     }
 }
+
+class Square extends Shape{
+    double sl; // Side Length
+    public Square(double s) {
+        sl = s;
+
+    }
+
+    @Override
+    public double area() {
+        return sl * sl;
+    }
+}
+
+class Triangle extends Shape{
+    double base;
+    double height;
+    public Triangle(double b, double h) {
+        base = b;
+        height = h;
+    }
+
+    @Override
+    public double area(){
+        return ((double) 1 /2) * base * height;
+    }
+}
 public class areaCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Which shapes area you want to calculate?");
         System.out.println("1. Circle");
         System.out.println("2. Rectangle");
+        System.out.println("3. Square");
+        System.out.println("4. Triangle");
         int shape = scanner.nextInt();
         switch (shape) {
             case 1 :
@@ -54,6 +85,22 @@ public class areaCalculator {
                 double l = scanner.nextInt();
                 Rectangle myRect = new Rectangle(l,w);
                 System.out.println("Rectangle area: " + myRect.area());
+                break;
+
+            case 3 :
+                System.out.println("Enter width: ");
+                double s = scanner.nextInt();
+                Square mySquare = new Square(s);
+                System.out.println("Rectangle area: " + mySquare.area());
+                break;
+
+            case 4 :
+                System.out.println("Enter base: ");
+                double b = scanner.nextInt();
+                System.out.println("Enter height: ");
+                double h = scanner.nextInt();
+                Triangle myTriangle = new Triangle(b , h);
+                System.out.println("Rectangle area: " + myTriangle.area());
                 break;
         }
     }
